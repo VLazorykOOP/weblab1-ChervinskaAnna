e0e5f420bb58b66d4109365220f199e5f702e68b
ChervinskaAnna <chervinska.anna@chnu.edu.ua>
2023-11-04 21:43:03 +0200

    LAB7 final

---
+++ js/myscript.js
@@ -0,0 +1,24 @@
+function handleLoad() {
+    alert('Сторінка повністю завантажена.');
+    // Ваш код для обробки події onload
+}
+
+function handleUnload() {
+    alert('Користувач виходить з документа.');
+    // Ваш код для обробки події onunload
+}
+
+function handleAfterPrint() {
+    alert('Документ роздрукований.');
+    // Ваш код для обробки події onafterprint
+}
+
+function handleBeforePrint() {
+    alert('Документ буде роздрукований.');
+    // Ваш код для обробки події onbeforeprint
+}
+
+function handleBeforeOnload() {
+    alert('Скрипт буде викликаний до завантаження документа.');
+    // Ваш код для обробки події onbeforeonload
+}
